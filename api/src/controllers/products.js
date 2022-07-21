5d81dbf2172f8f0c75d895c34c5159b3ac66df80
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-20 23:28:05 -0300

    feat - /products (GET & POST) working

--- api/src/controllers/products.js
+++ api/src/controllers/products.js
@@ -1,25 +1,36 @@
 const router = require("express").Router();
-const { getProducts } = require("../database/queries");
+const { creators, selectors } = require("../database/queries");
 
 router.get("/products", async (req, res) => {
   try {
-    const products = await getProducts();
-    res.json({ message: "get products", status: 200, products });
+    const { id } = req.query;
+    const products = !id
+      ? await selectors.selectProducts()
+      : await selectors.selectProductById({ id });
+    res.json({ message: "get product(s)", status: 200, data: products });
   } catch (e) {
-    res.status(500).json({ error: e.message, status: 500, products: [] });
+    res.status(400).json({ error: e.message, status: 400, data: null });
   }
 });
 
-router.post("/products", (req, res) => {
-  res.json({ message: "post products" });
+router.post("/products", async (req, res) => {
+  try {
+    const { product, brand } = req.body;
+    const createdProduct = await creators.createProduct({ product, brand });
+    res
+      .status(201)
+      .json({ message: "product created", status: 201, data: createdProduct });
+  } catch (e) {
+    res.status(400).json({ error: e.message, status: 400, data: null });
+  }
 });
 
 router.put("/products", (req, res) => {
-  res.json({ message: "put products" });
+  res.json({ message: "product updated" });
 });
 
 router.delete("/products", (req, res) => {
-  res.json({ message: "delete products" });
+  res.json({ message: "product deleted" });
 });
 
 module.exports = { products: router };
