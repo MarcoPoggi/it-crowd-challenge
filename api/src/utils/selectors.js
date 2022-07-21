a1345593900c80285266e2c6c7bfe4f6c9cdda5b
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-21 20:04:29 -0300

    rm - remove unused query params

--- api/src/utils/selectors.js
+++ api/src/utils/selectors.js
@@ -1,14 +1,9 @@
 const { Product, Brand } = require("../database/relations").models;
 
 //return all products
-const selectProducts = async (page = null, amount = false) => {
+const selectProducts = async () => {
   try {
-    if (amount === "all") return await Product.count();
-
-    let products = null;
-    (typeof Number(page) === "number") & (page >= 0)
-      ? (products = await Product.findAll({ offset: page * 10, limit: 10 }))
-      : (products = await Product.findAll({ include: Brand }));
+    const products = await Product.findAll({ include: Brand });
 
     return JSON.parse(JSON.stringify(products, null, 2));
   } catch (e) {
