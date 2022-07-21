0405b88e8e05b4acec2e1b236650f2d1161a8301
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-21 20:22:25 -0300

    fix - delete double slash on the url

--- client/src/services/products.js
+++ client/src/services/products.js
@@ -7,8 +7,8 @@ const endpoints = {
 
 export function getProducts(id, value) {
   const url = id
-    ? `${REACT_APP_API_URL}/${endpoints[id]}=${value}`
-    : `${REACT_APP_API_URL}/${endpoints.products}`;
+    ? `${REACT_APP_API_URL}${endpoints[id]}=${value}`
+    : `${REACT_APP_API_URL}${endpoints.products}`;
     
   return fetch(url)
     .then((res) => res.json())
