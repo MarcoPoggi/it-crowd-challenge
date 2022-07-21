9f6826ee2ed689d83bc695060e210c9c5565ad7c
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-20 23:30:26 -0300

    feat - add brands controller

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const { products } = require("../controllers/products");
 const { auth } = require("../controllers/auth");
+const { brands } = require("../controllers/brands");
 
-router.use("/api", products);
-router.use("/auth", auth);
+router.use("/api", [products, auth, brands]);
 
 module.exports = router;
