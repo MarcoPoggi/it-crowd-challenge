b890f6db46bbc73cb4c3fee49e4d6465e71412ba
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-21 20:06:36 -0300

    feat - add service for req products

---
+++ client/src/services/products.js
@@ -0,0 +1,17 @@
+const { REACT_APP_API_URL } = process.env;
+
+const endpoints = {
+  id: "api/products/?id",
+  products: "api/products",
+};
+
+export function getProducts(id, value) {
+  const url = id
+    ? `${REACT_APP_API_URL}/${endpoints[id]}=${value}`
+    : `${REACT_APP_API_URL}/${endpoints.products}`;
+    
+  return fetch(url)
+    .then((res) => res.json())
+    .then(({ data }) => data)
+    .catch((e) => e.message);
+}
