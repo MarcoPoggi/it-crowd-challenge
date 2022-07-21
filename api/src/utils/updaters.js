25a8094947cd4d3fc6c50685d7906bbcc29591df
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-21 00:32:36 -0300

    feat - possibility to update the brand (or not)

--- api/src/utils/updaters.js
+++ api/src/utils/updaters.js
@@ -1,14 +1,21 @@
-const { selectProductById } = require("./selectors");
+const { Product } = require("../database/relations").models;
+const { createBrand } = require("./creators");
 
 //return one product updated
 const updateProductById = async (id, { product = null, brand = null }) => {
   try {
-    const productToUpdate = await selectProductById({ id, model: true });
+    const productToUpdate = await Product.findByPk(id);
     if (!productToUpdate) throw new Error("the product does not exist");
+    const newBrand =
+      brand !== null
+        ? await createBrand({ brand, model: true, returnExistent: true })
+        : null;
 
-    !brand
-      ? productToUpdate.set({ ...product })
-      : productToUpdate.set({ ...product, brand });
+    if (!newBrand) productToUpdate.set({ ...product });
+    else {
+      productToUpdate.set({ ...product });
+      await productToUpdate.setBrand(newBrand);
+    }
 
     const productUpdated = await productToUpdate.save();
 
