90d6226de9d5e7ef84fac384f8341145481a3536
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-21 03:46:29 -0300

    feat - authorization path to sign access token

--- api/src/controllers/auth.js
+++ api/src/controllers/auth.js
@@ -1,7 +1,25 @@
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const { ADMIN, ADMIN_PASS, SECRET } = process.env;
 
-router.post("/authorization", (req, res) => {
-  res.json({ message: "authorization route" });
+router.post("/authorization", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password)
+      throw new Error("missing credentials(username or password)");
+    //simple validation
+    if (username !== ADMIN || password !== ADMIN_PASS)
+      throw new Error("invalid credentials");
+
+    const token = jwt.sign({ data: { username } }, SECRET, {
+      algorithm: "HS256",
+      expiresIn: "1h",
+    });
+
+    res.json({ message: "token sign", status: 200, token });
+  } catch (e) {
+    res.status(400).json({ error: e.message, status: 400, token: null });
+  }
 });
 
 module.exports = { auth: router };
