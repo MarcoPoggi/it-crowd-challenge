9d735c9f992df8e4fb1ae89d21cce7f008497f58
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-22 17:06:28 -0300

    feat - component & styles(CSS)

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,13 +1,25 @@
-import { SearchBar } from "../SearchBar/SearchBar";
+import { Link } from "wouter";
+import Logo from "../../assets/images/logoit-crowd.png";
+import styles from "./Header.module.css";
+import login_icon from "../../assets/images/login-icon.svg";
 
 export function Header() {
   return (
-    <header>
-      <div>
-        <img src="" alt="Logo" />
-        <button>Login</button>
+    <header className={styles.header_container}>
+      <div className={styles.content}>
+        <div className={styles.logo}>
+          <img src={Logo} alt="Logo" /> <h2>CHALLENGE</h2>
+        </div>
+        <div className={styles.redirect_container}>
+          <Link href="/login">
+            <img
+              src={login_icon}
+              alt="Login"
+              className={styles.redirect_image}
+            />
+          </Link>
+        </div>
       </div>
-      <SearchBar />
     </header>
   );
 }
