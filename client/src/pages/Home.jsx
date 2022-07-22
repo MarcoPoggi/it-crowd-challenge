0a304fb2a0edebe83362f90ee914065a6fc70abc
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-22 17:09:11 -0300

    del - remove provider

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,11 @@
 import { Header } from "../components/Header/Header";
 import { Products } from "../components/Products/Products";
-import { ResultsProvider } from "../contexts/ResultsContext";
 
 export function Home() {
   return (
     <div>
-      <ResultsProvider>
-        <Header />
-        <Products/>
-      </ResultsProvider>
+      <Header />
+      <Products />
     </div>
   );
 }
