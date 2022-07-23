94c435b860c3f44c9acc1ba72b96cdaee1c70413
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-23 19:13:48 -0300

    styles - render Logo/Avatar/FormLogin

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,17 @@
+import { Logo } from "../components/Logo/Logo";
+import { FormLogin } from "../components/Forms/LoginForm/FormLogin";
+import { Avatar } from "../components/Avatar/Avatar";
+import logo_img_cut from "../assets/images/logoit-crowd-cut.png";
+import admin_img from "../assets/images/admin.jpg";
+
 export function Login() {
   return (
     <div>
-      <h1>Login</h1>
+      <header>
+        <Logo image={logo_img_cut} />
+      </header>
+      <Avatar label={"Administrator"} image={admin_img} />
+      <FormLogin />
     </div>
   );
 }
