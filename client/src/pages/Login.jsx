da3097f8e7e47f31c4e899db3f4776291d759eef
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-23 20:52:49 -0300

    feat - add redirect in login component if authenticated, you cannot log in again until you close the session

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,15 +3,22 @@ import { FormLogin } from "../components/Forms/LoginForm/FormLogin";
 import { Avatar } from "../components/Avatar/Avatar";
 import logo_img_cut from "../assets/images/logoit-crowd-cut.png";
 import admin_img from "../assets/images/admin.jpg";
+import { AuthContext } from "../contexts/AuthContext";
+import { useContext } from "react";
+import { Redirect } from "wouter";
 
 export function Login() {
-  return (
+  const { authenticated, setAuthenticated } = useContext(AuthContext);
+
+  return !authenticated ? (
     <div>
       <header>
         <Logo image={logo_img_cut} />
       </header>
       <Avatar label={"Administrator"} image={admin_img} />
-      <FormLogin />
+      <FormLogin setAuth={setAuthenticated} />
     </div>
+  ) : (
+    <Redirect to="/" />
   );
 }
